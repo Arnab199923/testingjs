dd17d03e571be3d3c9a54eef161d6420854e4bfa
Arnab Deb <96194042+Arnab199923@users.noreply.github.com>
2022-10-13 21:54:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function fun(){
+   document.getElementById("btn")
+        document.getElementById("txt");
+        document.getElementById("head").innerHTML="heading added";
+        document.getElementById("para").innerHTML="random paragraph added";
+        document.getElementById("head").style.color="aqua";
+    
+}
+
+function newfun(){
+    document.getElementById("newbtn")
+    document.getElementById("head").innerHTML="heading changed";
+    document.getElementById("para").innerHTML="random paragraph changed";
+    document.getElementById("head").style.color="blue";
+        // document.getElementById("head").style.visibility="hidden";
+    }
+
+    function reloading(){
+        document.getElementById("close");
+        // window.close();
+        location.reload(true);
+    }
